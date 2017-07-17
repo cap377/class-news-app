48e6a24af7efbcea7533b5262df5a335fc144cc1
William Cortes <will.cortes@siriuscom.com>
2017-07-17 00:43:02 -0500

    add initial channel-feed and channel components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChannelFeedComponent } from './channel-feed/channel-feed.component';
+import { ChannelsComponent } from './channels/channels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelFeedComponent,
+    ChannelsComponent
   ],
   imports: [
     BrowserModule,
