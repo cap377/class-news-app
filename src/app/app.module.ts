b91d9562b6b3eb63adf6c60a2c72ac87d473ce9a
Scout Martinelli <scout.martinelli@siriuscom.com>
2017-07-17 13:28:00 -0500

    Added the service functionality and the test documents for it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlackService } from './services/slack.service';
+
+import { DatePipe } from '@angular/common';
+import { ServiceTestComponent } from './service-test/service-test.component';
+
+const appRoutes: Routes = [
+  {path: 'service-test', component: ServiceTestComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceTestComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
+  providers: [SlackService, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
